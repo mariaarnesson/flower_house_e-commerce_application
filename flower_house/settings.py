32f612bb1051813efa3b1c47bc5bd681ce5337b1
mariaarnesson <maria_arnesson@outlook.com>
2023-07-28 05:43:24 +0000

    Added templates index and mobile-top-header

--- flower_house/settings.py
+++ flower_house/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +60,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
