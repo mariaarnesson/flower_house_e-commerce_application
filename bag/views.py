985da7d0b70fdabc8c9beab1ab3126aa24eb8a52
mariaarnesson <maria_arnesson@outlook.com>
2023-07-29 11:52:24 +0000

    Updated remove from bag by removing size option in views.py app bag

--- bag/views.py
+++ bag/views.py
@@ -37,25 +37,15 @@ def adjust_bag(request, item_id):
     return redirect(reverse('view_bag'))
 
 
-
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
 
     try:
         product = get_object_or_404(Product, pk=item_id)
-        size = None
-        if 'product_size' in request.POST:
-            size = request.POST['product_size']
         bag = request.session.get('bag', {})
 
-        if size:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
-                bag.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
-        else:
-            bag.pop(item_id)
-            messages.success(request, f'Removed {product.name} from your bag')
+        bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
@@ -63,3 +53,4 @@ def remove_from_bag(request, item_id):
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
+
