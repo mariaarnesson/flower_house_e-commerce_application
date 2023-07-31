f8adfa89b29d270d0a1ce377f20c2de2f539c6ee
mariaarnesson <maria_arnesson@outlook.com>
2023-07-31 05:49:26 +0000

    Added checkout app and models

--- flower_house/settings.py
+++ flower_house/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'blog',
     'bag',
     'django_summernote',
+    'checkout',
 ]
 
 MIDDLEWARE = [
@@ -81,6 +82,8 @@ TEMPLATES = [
 ]
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+
 
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
