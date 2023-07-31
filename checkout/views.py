68dc45a84730382c8f90ba0a3a32bae55d889018
mariaarnesson <maria_arnesson@outlook.com>
2023-07-31 08:06:23 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,9 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MfsVlK1mRCHaetcYGfgefU76vTWLhLj7u5Qpk6T2fw5Zu5QOAbSxx1FkgypQ9y7lwid2tcjHSj3HKKbD7PUFyVt00eQ50LPUS',
+        'client_secret': 'sk_test_51MfsVlK1mRCHaetcS737ayvB9CiMj0U7CLI4mJRCkrYSEEAoCxabJeXrLXPpphUAWIA7pVrBaGTBt3GYssGAvT6n00rDFGhdtY'
+
     }
 
     return render(request, template, context)
