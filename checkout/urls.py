ddb673e2036a0a54fcb1f9c75de8fc9e42dc44c6
mariaarnesson <maria_arnesson@outlook.com>
2023-07-31 13:10:34 +0000

    Updated models and admin file with orginal_bag and stripe_pid

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,5 +5,6 @@ from .webhooks import webhook
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout/success/<str:order_number>/', views.checkout_success, name='checkout_success'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
     path('wh/', webhook, name="webhook"),
 ]
