c4377e7afe2e5dc5f4791b530aff8cc910349b35
mariaarnesson <maria_arnesson@outlook.com>
2023-08-01 08:09:47 +0000

    Added a custom 404 page

--- flower_house/urls.py
+++ flower_house/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .views import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -30,3 +31,4 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = 'flower_house.views.handler404'
