6554632145081ad3c3b70d85d56518912132bc2f
mariaarnesson <maria_arnesson@outlook.com>
2023-08-01 06:20:32 +0000

    Added category to blog

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_bag, name='view_bag'),
-    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
-    path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
-    path('remove/<item_id>/', views.remove_from_bag, name='remove_from_bag'),
+    path('', views.blog_category, name='blog_category'),
+    path('post_list', views.post_list, name='post_list'),
 ]
