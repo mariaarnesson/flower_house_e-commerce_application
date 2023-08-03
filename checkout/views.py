f2917835f0d740aea981d79bb27f1feefda22c8e
mariaarnesson <maria_arnesson@outlook.com>
2023-08-03 05:53:27 +0000

    Splitted up too long line on views.py checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.shortcuts import (
+    render,
+    redirect,
+    reverse,
+    get_object_or_404,
+    HttpResponse
+)
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from bag.contexts import bag_contents
@@ -12,13 +18,16 @@ from .models import Order, OrderLineItem
 import stripe
 import json
 
+
 @require_POST
 def cache_checkout_data(request):
     try:
         pid = request.POST.get('client_secret', None)
         if pid is None:
-            raise ValueError("The 'client_secret' field is missing from the request.")
-        
+            raise ValueError(
+                "The 'client_secret' field is missing from the request."
+            )
+
         pid = pid.split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
@@ -56,8 +65,10 @@ def checkout(request):
             order = order_form.save(commit=False)
             pid = request.POST.get('client_secret', None)
             if pid is None:
-                raise ValueError("The 'client_secret' field is missing from the request.")
-            
+                raise ValueError(
+                    "The 'client_secret' field is missing from the request."
+                )
+
             pid = pid.split('_secret')[0]
             order.stripe_pid = pid
             order.original_bag = json.dumps(bag)
@@ -75,28 +86,38 @@ def checkout(request):
                     else:
                         messages.error(request, (
                             "Invalid quantity for product {}. "
-                            "Please call us for assistance!".format(product.name))
+                            "Please call us for assistance!"
+                            .format(product.name)
+                            )
                         )
                         order.delete()
                         return redirect(reverse('view_bag'))
 
                 except Product.DoesNotExist:
-                    messages.error(request, (
-                        "One of the products in your bag wasn't found in our database. "
-                        "Please call us for assistance!")
+                    messages.error(
+                        request,
+                        (
+                         "One of the products in your bag"
+                         "wasn't found in our database."
+                         "Please call us for assistance!")
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
 
             request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_number]))
+            return redirect(reverse('checkout_success',
+                            args=[order.order_number]
+                            ))
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double-check your information.')
     else:
         bag = request.session.get('bag', {})
         if not bag:
-            messages.error(request, "There's nothing in your bag at the moment")
+            messages.error(
+                request,
+                "There's nothing in your bag at the moment"
+            )
             return redirect(reverse('products'))
 
         current_bag = bag_contents(request)
