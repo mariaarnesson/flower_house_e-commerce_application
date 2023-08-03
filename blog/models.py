0059ea4cd9ddfd62a690e0651e1817054cb0f382
mariaarnesson <maria_arnesson@outlook.com>
2023-08-03 05:25:57 +0000

    Made change to format text on blog_post class comment in models.py

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class BlogCategory(models.Model):
     name = models.CharField(max_length=100)
     image_url = models.URLField(max_length=1024,
                                 null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)    
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -23,7 +23,7 @@ class BlogPost(models.Model):
     )
     image_url = models.URLField(max_length=1024,
                                 null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)    
+    image = models.ImageField(null=True, blank=True)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
@@ -31,7 +31,7 @@ class BlogPost(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(
         User, related_name='blogpost_like', blank=True)
-    blog_category = models.ForeignKey(BlogCategory, on_delete=models.CASCADE)    
+    blog_category = models.ForeignKey(BlogCategory, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ["-created_on"]
@@ -44,8 +44,11 @@ class BlogPost(models.Model):
 
 
 class Comment(models.Model):
-    blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE,
-                             related_name="comments")
+    blog_post = models.ForeignKey(
+        BlogPost,
+        on_delete=models.CASCADE,
+        related_name="comments"
+        )
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
