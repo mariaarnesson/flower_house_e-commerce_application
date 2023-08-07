c5b7d4143be67f7ede55ea4dd54062f3edc0b6dd
mariaarnesson <maria_arnesson@outlook.com>
2023-08-07 14:25:19 +0000

    updated handle payment intent succeeded

--- flower_house/settings.py
+++ flower_house/settings.py
@@ -229,13 +229,13 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'marys.zieminska@gmail.com'
 
 if 'DEVELOPMENT' in os.environ:
-   EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-   DEFAULT_FROM_EMAIL = 'marys.zieminska@gmail.com'
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'marys.zieminska@gmail.com'
 else:
-   EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-   EMAIL_USE_TLS = True
-   EMAIL_PORT = 587
-   EMAIL_HOST = 'smtp.gmail.com'
-   EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-   EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-   DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
