0e31ac71b03ad3e5fb3c2eeb1bc6ed52c10154e1
mariaarnesson <maria_arnesson@outlook.com>
2023-08-07 12:33:29 +0000

    Added secret key

--- flower_house/settings.py
+++ flower_house/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l+m!=182mxiin)tf1)qt+r)w1q$$ma8_s)wvcl-@kl8hgye++('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
